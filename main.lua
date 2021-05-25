493ca7a118148bd3b69c9852f2ed8add007825bb
DPS2004 <iheartscratch@gmail.com>
2021-05-25 09:34:58 -0400

    draw ditto

--- main.lua
+++ main.lua
@@ -3,6 +3,18 @@ function love.load()
   
   maincanvas = love.graphics.newCanvas(gameWidth, gameHeight)
   
+  ditto = {
+    spr = love.graphics.newImage("assets/ditto.png"),
+    frame = 1,
+    quads = {},
+    delay = 6,
+  }
+  
+  for i=0,3 do
+    ditto.quads[i+1] = love.graphics.newQuad(i*33,0,33,29,132,29)
+    
+  end
+  
   font = love.graphics.newFont("assets/Axmolotl.ttf",16)
   font:setFilter("nearest","nearest")
   love.graphics.setFont(font)
@@ -13,9 +25,17 @@ end
 
 function love.update()
   
+  ditto.delay = ditto.delay - 1
+  if ditto.delay <= 0 then
+    ditto.delay = ditto.delay + 6
+    ditto.frame = (ditto.frame)%4 + 1
+
+  end
+  
   if not source:isPlaying( ) then
 		love.audio.play( source )
 	end
+  
   
   
 end
@@ -26,7 +46,7 @@ function love.draw()
   love.graphics.setCanvas(maincanvas)
     love.graphics.clear()
     love.graphics.print("hello world!",0,0,0,2,2)
-    
+    love.graphics.draw(ditto.spr,ditto.quads[ditto.frame],64,64)
     
   love.graphics.setCanvas()
   love.graphics.clear()
